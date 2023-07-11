b7393915ea24d5df7b62e184c5c73d7ddd25681b
Saud <saud.sadiq@salesassist.pk>
2023-07-11 15:18:56 +0500

    component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,12 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './screen/home/home.component';
 import { HeaderComponent } from '../shared/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductSliderComponent } from './components/product-slider/product-slider.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-
+    ProductSliderComponent,
+    
   ],
   imports: [
     CommonModule,
